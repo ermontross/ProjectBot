c60ea422c72a886fc205719d3343e11923b0e8e7
ermontross <emma.montross@colorado.edu>
2017-02-25 23:36:24 -0700

    working on stand-up ability

--- bot/messenger.py
+++ bot/messenger.py
@@ -82,5 +82,6 @@ class Messenger(object):
         self.commitments[user_id] = msg_txt
 
     def write_commitments(self, channel_id, user_id):
+        user_id = user_id[1:]
         txt = "Your stand-up message today: " + self.commitments[user_id]
         self.send_message(channel_id, txt)
