605ae87b46206702dd84e616467407c4beaa0bac
ermontross <emma.montross@colorado.edu>
2017-02-25 18:54:12 -0700

    resetting things

--- bot/messenger.py
+++ bot/messenger.py
@@ -1,32 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-
 import logging
 import random
-
-import httplib2
-import os
-
-from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
-
-import datetime
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/calendar-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Calendar API Python Quickstart'
-
 
 logger = logging.getLogger(__name__)
 
@@ -34,35 +9,6 @@ logger = logging.getLogger(__name__)
 class Messenger(object):
     def __init__(self, slack_clients):
         self.clients = slack_clients
-        self.gcal_credentials = None
-
-    def get_credentials(self):
-        """Gets valid user credentials from storage.
-
-        If nothing has been stored, or if the stored credentials are invalid,
-        the OAuth2 flow is completed to obtain the new credentials.
-
-        Returns:
-            sets the global credentials variable to the credentials for Emma's colorado.edu google account
-        """
-        home_dir = os.path.expanduser('~')
-        credential_dir = os.path.join(home_dir, '.credentials')
-        if not os.path.exists(credential_dir):
-            os.makedirs(credential_dir)
-        credential_path = os.path.join(credential_dir,
-                                       'calendar-python-quickstart.json')
-
-        store = Storage(credential_path)
-        credentials = store.get()
-        if not credentials or credentials.invalid:
-            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-            flow.user_agent = APPLICATION_NAME
-            if flags:
-                credentials = tools.run_flow(flow, store, flags)
-            else:  # Needed only for compatibility with Python 2.6
-                credentials = tools.run(flow, store)
-            print('Storing credentials to ' + credential_path)
-        self.gcal_credentials = credentials
 
     def send_message(self, channel_id, msg):
         # in the case of Group and Private channels, RTM channel payload is a complex dictionary
@@ -116,24 +62,3 @@ class Messenger(object):
             "color": "#7CD197",
         }
         self.clients.web.chat.post_message(channel_id, txt, attachments=[attachment], as_user='true')
-
-    def get_schedule(self, channel_id):
-        msg = ""
-        http = self.gcal_credentials.authorize(httplib2.Http())
-        service = discovery.build('calendar', 'v3', http=http)
-
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(
-            calendarId='colorado.edu_evtfbhg3rqiihrcgple6m9q558@group.calendar.google.com', timeMin=now, maxResults=10,
-            singleEvents=True,
-            orderBy='startTime').execute()
-        events = events_result.get('items', [])
-
-        if not events:
-            msg = 'No upcoming events found.'
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            msg = start + '\n' + event['summary']
-
-        self.send_message(channel_id, msg)
